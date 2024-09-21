6a95e47b0fc66728ec02ada6b879c948dc16f885
Octavius Tan <otan@andrew.cmu.edu>
2024-09-21 18:52:42 -0400

    added error pin message logic

--- src/posts/tools.js
+++ src/posts/tools.js
@@ -13,6 +13,10 @@ module.exports = function (Posts) {
 		return await togglePostDelete(uid, pid, false);
 	};
 
+	Posts.tools.pin = async function (pid, duration) {
+        return await togglePostPin(pid, true, duration);
+    };
+
 	async function togglePostDelete(uid, pid, isDelete) {
 		const [postData, canDelete] = await Promise.all([
 			Posts.getPostData(pid),
@@ -41,4 +45,33 @@ module.exports = function (Posts) {
 		}
 		return post;
 	}
+
+	async function togglePostPin(pid, isPinned, postPinDuration) {
+        const [postData, canPin] = await Promise.all([
+            Posts.getPostData(pid),
+            privileges.posts.canPin(pid),
+        ]);
+        if (!postData) {
+            throw new Error('[[error:no-post]]');
+        }
+
+		// logic to check whether the post is already pinned
+        if (isPinned && postData.pinned) {
+            throw new Error('[[error:post-already-pinned]]');
+        } else if (!isPinned && !postData.pinned) {
+            throw new Error('[[error:post-already-unpinned]]');
+        }
+
+        if (!canPin.flag) {
+            throw new Error(canPin.message);
+        }
+
+        let post;
+        if (isPinned) {
+            post = await Posts.pin(pid, postPinDuration);
+        } else {
+            post = await Posts.unpin(pid);
+        }
+        return post;
+    }
 };
