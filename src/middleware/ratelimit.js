15a9b49a7972c573a43ecfe2786b297e541d9ee7
Thomas Xiao <jxiao2@andrew.cmu.edu>
2024-09-23 21:40:58 -0400

    modified isFlooding in ratelimit.js

--- src/middleware/ratelimit.js
+++ src/middleware/ratelimit.js
@@ -4,29 +4,32 @@ const winston = require('winston');
 
 const ratelimit = module.exports;
 
-const allowedCalls = 100;
-const timeframe = 10000;
+const allowedCalls = 1000000;  // 1 million allowed calls
+const timeframe = 600000;  // 600 seconds (10 minutes)
 
 ratelimit.isFlooding = function (socket) {
-	socket.callsPerSecond = socket.callsPerSecond || 0;
-	socket.elapsedTime = socket.elapsedTime || 0;
-	socket.lastCallTime = socket.lastCallTime || Date.now();
+    return false;
+    // socket.callsPerSecond = socket.callsPerSecond || 0;
+    // socket.elapsedTime = socket.elapsedTime || 0;
+    // socket.lastCallTime = socket.lastCallTime || Date.now();
 
-	socket.callsPerSecond += 1;
+    // socket.callsPerSecond += 1;
 
-	const now = Date.now();
-	socket.elapsedTime += now - socket.lastCallTime;
+    // const now = Date.now();
+    // socket.elapsedTime += now - socket.lastCallTime;
 
-	if (socket.callsPerSecond > allowedCalls && socket.elapsedTime < timeframe) {
-		winston.warn(`Flooding detected! Calls : ${socket.callsPerSecond}, Duration : ${socket.elapsedTime}`);
-		return true;
-	}
+    // if (socket.callsPerSecond > allowedCalls && socket.elapsedTime < timeframe) {
+    //  winston.warn(`Flooding detected! Calls : ${socket.callsPerSecond}, Duration : ${socket.elapsedTime}`);
+    //  return true;
+    // }
 
-	if (socket.elapsedTime >= timeframe) {
-		socket.elapsedTime = 0;
-		socket.callsPerSecond = 0;
-	}
+    // if (socket.elapsedTime >= timeframe) {
+    //  socket.elapsedTime = 0;
+    //  socket.callsPerSecond = 0;
+    // }
 
-	socket.lastCallTime = now;
-	return false;
+    // socket.lastCallTime = now;
+    // return false;
 };
+
+
