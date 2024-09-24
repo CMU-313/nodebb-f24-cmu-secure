d1b146ef865ab82b21f99c966ec581053954a92f
Thomas Xiao <jxiao2@andrew.cmu.edu>
2024-09-24 19:56:21 -0400

    Made changes to ratelimit.js and chats.js for lint issues try second run.

--- src/middleware/ratelimit.js
+++ src/middleware/ratelimit.js
@@ -1,29 +1,23 @@
 'use strict';
 
 const winston = require('winston');
-
 const ratelimit = module.exports;
-
-const allowedCalls = 1000000;  // 1 million allowed calls
-const timeframe = 600000;  // 600 seconds (10 minutes)
+const allowedCalls = 1000000;
+const timeframe = 600000;
 
 ratelimit.isFlooding = function (socket) {
-
-    socket.callsPerSecond = socket.callsPerSecond || 0;
-    socket.elapsedTime = socket.elapsedTime || 0;
-    socket.lastCallTime = socket.lastCallTime || Date.now();
-
-    socket.callsPerSecond += 1;
-
-    const now = Date.now();
-    socket.elapsedTime += now - socket.lastCallTime;
-
-    if (socket.elapsedTime >= timeframe) {
-     socket.elapsedTime = 0;
-     socket.callsPerSecond = 0;
-    }
-    socket.lastCallTime = now;
-    return false;
+	socket.callsPerSecond = socket.callsPerSecond || 0;
+	socket.elapsedTime = socket.elapsedTime || 0;
+	socket.lastCallTime = socket.lastCallTime || Date.now();
+	socket.callsPerSecond += 1;
+	const now = Date.now();
+	socket.elapsedTime += now - socket.lastCallTime;
+	if (socket.elapsedTime >= timeframe) {
+	 socket.elapsedTime = 0;
+	 socket.callsPerSecond = 0;
+	}
+	socket.lastCallTime = now;
+	return false;
 };
 
 
